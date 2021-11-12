88dfacfaf016377c72ca1bf1307d8eb76cebfc6c
ermangurses <egurses@email.arizona.edu>
2021-11-12 09:55:21 -0500

    Modify assignment two

--- assignment2/assignment2.cu
+++ assignment2/assignment2.cu
@@ -7,81 +7,118 @@
 #include <stdbool.h>
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, 
-                                                              bool abort=true){
-   if (code != cudaSuccess){
-      fprintf(stderr,"GPUassert: %s %s %d\n", 
-                                         cudaGetErrorString(code), file, line);
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n",cudaGetErrorString(code), file, line);
       if (abort) exit(code);
    }
 }
 
-__global__ void filter(float * device_dataIn)
+__global__ void sum_GPU(float * device_dataIn1, float * device_dataIn2, float * device_dataIn3)
 {
-    int blockId = blockIdx.x + blockIdx.y * gridDim.x + gridDim.x * gridDim.y * blockIdx.z;
-
-    int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
-                   + (threadIdx.z * (blockDim.x * blockDim.y))
-                   + (threadIdx.y * blockDim.x) + threadIdx.x;
-
-    printf("threadId %d has %f as data \n",threadId,device_dataIn[threadId]);
-
+   int threadId = threadIdx.x + blockIdx.x * blockDim.x;
+   device_dataIn1[threadId] = device_dataIn1[threadId] + device_dataIn2[threadId] + device_dataIn3[threadId];
 }
 
-int main(int argc, char* argv[]) {
+void sum_CPU(float* host_dataCPU, float* host_dataIn1, float* host_dataIn2, float*  host_dataIn3, int DIM_X)
+{
+    for ( int x = 0; x < DIM_X; ++x )
+    {
+        host_dataCPU[x] = host_dataIn1[x] + host_dataIn2[x] + host_dataIn3[x];
+    }
+}
+
+int main(int argc, char* argv[]) 
+{
 
   // Dimensions of the 3D data
-  int DIM_X = 4;
-  int DIM_Y = 4;
-  int DIM_Z = 4;
+  int DIM_X = 4194304;
 
   // Definition of host and device pointers
-  float * host_dataIn;
-  float * device_dataIn;
+  float * host_dataIn1, * host_dataIn2, * host_dataIn3, * host_dataCPU, * host_dataGPU;
+  float * device_dataIn1, * device_dataIn2, * device_dataIn3;
 
   // Memory allocation for host data
-  host_dataIn = new float [DIM_X * DIM_Y * DIM_Z];
-  
-  // Calculation of required spaces 
-  const int DATA_BYTES      = (DIM_X * DIM_Y * DIM_Z) * sizeof(float);
+  host_dataIn1 = new float [DIM_X];
+  host_dataIn2 = new float [DIM_X];
+  host_dataIn3 = new float [DIM_X];
+  host_dataGPU = new float [DIM_X];
+  host_dataCPU = new float [DIM_X];
 
-  int i = 0;
+
+  // Calculation of required spaces 
+  const int DATA_BYTES  = (DIM_X) * sizeof(float);
   // Initialize host_dataIn with random numbers
-  for( int x = 0; x < DIM_X; ++x )
+  for ( int x = 0; x < DIM_X; ++x)
   {
-      for(int y = 0; y < DIM_Y; ++y )
-      {
-          for(int z = 0; z < DIM_Z; ++z )
-	  { 
-              host_dataIn [z + y * DIM_Z + x * DIM_Z * DIM_Y] = i++; 
-                                          // (rand() / (float)RAND_MAX * 100) + 1;
-          }
-      }
+      host_dataIn1 [x] = (rand() / (float)RAND_MAX * 100) + 1;
+      host_dataIn2 [x] = (rand() / (float)RAND_MAX * 100) + 1;
+      host_dataIn3 [x] = (rand() / (float)RAND_MAX * 100) + 1;
   }
 
+  clock_t cpu_start = clock();
+  sum_CPU(host_dataCPU, host_dataIn1, host_dataIn2, host_dataIn3, DIM_X);
+  clock_t cpu_end = clock();
+
   // Allocate DEVICE arrays on GPU for dataIn
-  gpuErrchk( cudaMalloc( ( void**) &device_dataIn,  DATA_BYTES));
-  gpuErrchk( cudaMemcpy( device_dataIn, host_dataIn, DATA_BYTES, cudaMemcpyHostToDevice));
-  int blockX = 2;
-  int blockY = 2;
-  int blockZ = 2;
-  dim3 blockSize(blockX,blockY,blockZ);
+  gpuErrchk( cudaMalloc( ( void**) &device_dataIn1,  DATA_BYTES));
+  gpuErrchk( cudaMalloc( ( void**) &device_dataIn2,  DATA_BYTES));
+  gpuErrchk( cudaMalloc( ( void**) &device_dataIn3,  DATA_BYTES));
+
+  clock_t htod_start, htod_end, dtoh_start, dtoh_end;
+  clock_t gpu_start, gpu_end;
+
+  htod_start = clock();
+  gpuErrchk( cudaMemcpy( device_dataIn1, host_dataIn1, DATA_BYTES, cudaMemcpyHostToDevice));
+  gpuErrchk( cudaMemcpy( device_dataIn2, host_dataIn2, DATA_BYTES, cudaMemcpyHostToDevice));
+  gpuErrchk( cudaMemcpy( device_dataIn3, host_dataIn3, DATA_BYTES, cudaMemcpyHostToDevice));
+  htod_end = clock();
+
+  int blockX = 64;
+  dim3 blockSize(blockX);
 
   int gridX = (DIM_X) / blockX;
-  int gridY = (DIM_Y) / blockY; 
-  int gridZ = (DIM_Z) / blockZ;
-  dim3 gridSize(gridX,gridY,gridZ);
+  dim3 gridSize(gridX);
+
+  gpu_start = clock();
+  ///////////////////////////////////////////////////////////////////////////////
+  // Kernel Call
+  ///////////////////////////////////////////////////////////////////////////////
+  sum_GPU<<<gridSize,blockSize>>>(device_dataIn1, device_dataIn2, device_dataIn3);
+  ///////////////////////////////////////////////////////////////////////////////
+  gpu_end = clock();
+
+  dtoh_start = clock();
+  gpuErrchk( cudaMemcpy(host_dataGPU, device_dataIn1, DATA_BYTES, cudaMemcpyDeviceToHost));
+  dtoh_end = clock();
+
+  printf("Sum array CPU execution time : %4.6f \n",
+		  (double)((double)(cpu_end - cpu_start) / CLOCKS_PER_SEC));
+
+  printf("Sum array GPU execution time : %4.6f \n",
+                  (double)((double)(gpu_end - gpu_start) / CLOCKS_PER_SEC));
+
+  printf("htod mem transfer time : %4.6f \n",
+                  (double)((double)(htod_end - htod_start) / CLOCKS_PER_SEC));
+
+  printf("dtoh mem transfer time : %4.6f \n",
+                  (double)((double)(dtoh_end - dtoh_start) / CLOCKS_PER_SEC));
+
+  printf("Sum array GPU total execution time : %4.6f \n",
+                  (double)((double)(dtoh_end - htod_start) / CLOCKS_PER_SEC));
 
 
-///////////////////////////////////////////////////////////////////////////////
-// Kernel Call
-///////////////////////////////////////////////////////////////////////////////
-  filter<<<gridSize,blockSize>>>(device_dataIn);
-///////////////////////////////////////////////////////////////////////////////
+  cudaFree( device_dataIn1 );
+  cudaFree( device_dataIn2 );
+  cudaFree( device_dataIn3 );
 
+  delete[] host_dataIn1;
+  delete[] host_dataIn2;
+  delete[] host_dataIn3;
+  delete[] host_dataGPU;
+  delete[] host_dataCPU;
 
-
-  cudaFree( device_dataIn );
-  delete[] host_dataIn;
   return 0;
 }
